4deee435844b00573469ffc9a8a01ee2bf41fad7
Austin <65206545+Austin-HTTPS@users.noreply.github.com>
2022-05-27 02:58:44 +0000

    fixed the image in the header

--- config.js
+++ config.js
@@ -61,7 +61,7 @@ module.exports = {
         position: `Founder`
     }],
     header: {
-        icon: `illustration-night-mountains-stars-wallpaper_3500-1969-1539560122.png`,
+        icon: `5f459e3acca1158310a937e73203f8a1.png`,
         name: `Austin`,
         buttons: {
             Discord: `/discord`,
