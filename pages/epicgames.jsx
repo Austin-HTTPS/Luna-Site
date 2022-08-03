c4999c85871b5fb549ae26f632d56730da36bcbb
Austin <65206545+Austin-HTTPS@users.noreply.github.com>
2022-08-03 00:45:35 +0000

    a

--- pages/epicgames.jsx
+++ pages/epicgames.jsx
@@ -10,7 +10,12 @@ function EpicGames() {
                 <title>Austin IS Pog</title>
             </Head>
             <body>
-            <p>Hey! Since Epic Games doesn't have a profile link, I can't redirect you anywhere :(; however, I can give you my username. My Epic Games username is NotMrAustin .</p>
+              <div className={style.align}>
+                    <div className={style.container}>
+                        <strong style={{ fontSize: 32, color: '#ddd9e6' }}>Epic Games</strong><text style={{ color: 'rgb(65, 59, 74)', fontSize: 24 }}></text> <br />
+                        <p>Hey! Since Epic Games doesn't have a profile link, I can't redirect you anywhere :(; however, I can give you my username. My Epic Games username is NotMrAustin .</p>
+                    </div>
+              </div>
             </body>
         </div>
     );
