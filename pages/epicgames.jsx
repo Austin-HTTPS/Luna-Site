4928c68ef657091beb65b720bd6fc2cc5a195c41
Austin <65206545+Austin-HTTPS@users.noreply.github.com>
2022-08-03 01:08:07 +0000

    testing out the epicgames page

--- pages/epicgames.jsx
+++ pages/epicgames.jsx
@@ -12,7 +12,7 @@ function EpicGames() {
             </Head>
               <div className={style.align}>
                     <div className={style.container}>
-                        <strong style={{ fontSize: 32, color: '#ddd9e6' }}>Epic Games</strong><text style={{ color: 'rgb(65, 59, 74)', fontSize: 24 }}>Hey! Since Epic Games doesn't have a profile link, I can't redirect you anywhere :(; however, I can give you my username. My Epic Games username is NotMrAustin .</text> <br />
+                        <strong style={{ fontSize: 32, color: '#ddd9e6' }}>Epic Games</strong><text style={{ color: 'rgb(221, 217, 230)', fontSize: 24 }}> ⌋ Hey! Since Epic Games doesn't have a profile link, I can't redirect you anywhere :(; however, I can give you my username. My Epic Games username is <strong>NotMrAustin</strong>.</text> <br />
                     </div>
               </div>
         </div>
