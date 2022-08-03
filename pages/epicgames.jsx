559f8c57a6b830eafdfbb0e81266c6bff26a83fc
Austin <65206545+Austin-HTTPS@users.noreply.github.com>
2022-08-03 23:53:28 +0000

    die

--- pages/epicgames.jsx
+++ pages/epicgames.jsx
@@ -8,11 +8,11 @@ function EpicGames() {
             <Head>
                 <meta charSet="utf-8" />
                 <meta name="viewport" content="width=device-width" />
-                <title>Austin - EpicGames</title>
+                <title>Austin - Epicgames</title>
             </Head>
               <div className={style.align}>
                     <div className={style.container}>
-                        <strong style={{ fontSize: 32, color: '#ddd9e6' }}>Epic Games</strong><text style={{ color: 'rgb(221, 217, 230)', fontSize: 24 }}> ⌋ Hey! Since Epic Games doesn't have a profile link, I can't redirect you anywhere :(; however, I can give you my username. My Epic Games username is <strong>NotMrAustin</strong>.</text> <br />
+                        <strong style={{ fontSize: 32, color: '#ddd9e6' }}>Epicgames</strong><text style={{ color: 'rgb(221, 217, 230)', fontSize: 24 }}> ⌋ Hey! Since Playstation doesn't have a profile link, I can't redirect you anywhere :(; however, I can give you my username. My Epicgames username is <strong>NotMrAustin</strong>.</text> <br />
                     </div>
               </div>
         </div>
