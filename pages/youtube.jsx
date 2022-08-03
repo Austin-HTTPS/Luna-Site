a7e21b0a05643b29593b1afd7eb4d8a83db82c54
Austin <65206545+Austin-HTTPS@users.noreply.github.com>
2022-08-03 02:19:37 +0000

    PLZ FIX

--- pages/youtube.jsx
+++ pages/youtube.jsx
@@ -9,9 +9,9 @@ function YouTube() {
                 <meta charSet="utf-8" />
                 <meta name="viewport" content="width=device-width" />
                 <title>Austin - YouTube</title>
-                <meta httpEquiv="refresh" content="0; url=" />
+                <meta httpEquiv="refresh" content="0; url=https://youtube.com/notmraustin" />
             </Head>
-          <div className={style.align}>
+                <div className={style.align}>
                     <div className={style.container}>
                         <strong style={{ fontSize: 32, color: '#ddd9e6' }}>YouTube</strong><text style={{ color: 'rgb(65, 59, 74)', fontSize: 24 }}> ⌋ Hey! It looks like you have ran in an <strong>error</strong>! King <strong>Bob</strong> and his minions have luckily found the url that you were supposed to be redirected to. Please click on the following link to be directed to your destination: <strong><a href="https://youtube.com/notmraustin">https://youtube.com/notmraustin</a></strong></text> <br />
                     </div>
