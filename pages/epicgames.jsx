581e8786ecb9dddd3c0b4711c20d838e3e56851f
Austin <65206545+Austin-HTTPS@users.noreply.github.com>
2022-08-03 00:39:12 +0000

    added more stuff

---
+++ pages/epicgames.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import Head from 'next/head';
+
+function EpicGames() {
+    return (
+        <div>
+            <Head>
+                <meta charSet="utf-8" />
+                <meta name="viewport" content="width=device-width" />
+                <title>Austin IS Pog</title>
+            </Head>
+            <body>
+            <p>Hey! Since Epic Games doesn't have a profile link, I can't redirect you anywhere :(; however, I can give you my username. My Epic Games username is NotMrAustin .</p>
+            </body>
+        </div>
+    );
+}
+
+export default EpicGames;
--- pages/index.jsx
+++ pages/index.jsx
@@ -2,7 +2,7 @@ import config from '../config';
 import style from '../styles/Home.module.css';
 import { useState, useEffect } from 'react';
 
-import { HiOutlineNewspaper, HiOutlineLink, HiX, HiOutlineExclamation, HiOutlineFolder, HiOutlineMenuAlt2, HiVideoCamera, HiOutlineCode } from 'react-icons/hi';
+import { HiOutlineNewspaper, HiOutlineLink, HiX, HiOutlineExclamation, HiOutlineFolder, HiOutlineMenuAlt2, HiVideoCamera, HiOutlineShieldCheck, HiOutlineCode } from 'react-icons/hi';
 export default function Homepage({ user, cards, error }) {
     const [width, setWidth] = useState()
     useEffect(() => {
@@ -34,8 +34,8 @@ export default function Homepage({ user, cards, error }) {
                             }
                         </div>
                         <div className={style.badges} style={(user?.status?.emote || user?.status?.text) ? { marginTop: 8 } : { marginTop: width > 540 ? 14 : 4 }}>
-                            <button className={style.badge} title='https://austinispog.tk/youtube' onClick={() => window.open('https://austinispog.tk/youtube')}> <HiVideoCamera style={{ height: 22, width: 22, marginRight: 4 }} /> YouTuber </button>
-                            <button className={style.badge} title='https://austinispog.tk/github' onClick={() => window.open('https://austinispog.tk/github')}> <HiOutlineCode style={{ height: 22, width: 22, marginRight: 4 }} /> Developer </button>
+                            <button className={style.badge} title='https://austinispog.tk/youtube' onClick={() => window.open('https://austinispog.tk/youtube')}> <HiVideoCamera style={{ height: 22, width: 22, marginRight: 4 }} /> Content Creator </button>
+                            <button className={style.badge} title='https://austinispog.tk/discord' onClick={() => window.open('https://austinispog.tk/discord')}> <HiOutlineShieldCheck style={{ height: 22, width: 22, marginRight: 4 }} /> Moderator </button>
                         </div>
                     </div>
                     <div className={style.content}>
