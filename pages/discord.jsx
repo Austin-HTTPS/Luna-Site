2e6e330554091baf356abcf276a2537c9803fac0
Austin <65206545+Austin-HTTPS@users.noreply.github.com>
2022-08-04 00:07:45 +0000

    w

--- pages/discord.jsx
+++ pages/discord.jsx
@@ -1,7 +1,17 @@
-export default {
-  <div>
-    <meta charSet="utf-8" />
-    <meta name="viewport" content="width=device-width" />
-    <title>Austin IS Pog</title>
-    <meta httpEquiv="refresh" content="0; url=https://example.com" />
-  </div>}
+import React from 'react';
+import Head from 'next/head';
+
+function Discord() {
+    return (
+        <div>
+            <Head>
+                <meta charSet="utf-8" />
+                <meta name="viewport" content="width=device-width" />
+                <title>Austin IS Pog</title>
+                <meta httpEquiv="refresh" content="0; url=https://discord.gg/a3av4pZMx2" />
+            </Head>
+        </div>
+    );
+}
+
+export default Discord; 
