e268f9044d8cb2d12dd23517a86e04b85c6c9fe6
Austin-HTTPS <65206545+Austin-HTTPS@users.noreply.github.com>
2023-04-15 04:40:03 +0000

    changed status

--- config.js
+++ config.js
@@ -15,7 +15,7 @@ module.exports = {
                 color: "#ED4245"
             },
             emote: "status.gif",
-            text: "if you only do what you can do, you will never be more than you are now"
+            text: "if there is a hole, there is a goal - masteroogwgay"
         },
         activities: [],
         createdTimestamp: 1615924806867,
