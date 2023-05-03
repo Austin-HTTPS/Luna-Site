a5a123ac3204cdbf13c3b3c2b072c727c510307d
Austin-HTTPS <65206545+Austin-HTTPS@users.noreply.github.com>
2023-05-03 05:43:14 +0000

    :)

--- config.js
+++ config.js
@@ -1,79 +1,79 @@
 module.exports = {
-    api: {
-        url: 'https://whois.mraustin.net',
-        path: '/'
+  api: {
+    url: 'https://whois.mraustin.net',
+    path: '/'
+  },
+  user: {
+    id: "599336348381609984",
+    username: "MrAustin",
+    discriminator: "3915",
+    nickname: "Austin",
+    nickavatar: "pfpss2.png",
+    status: {
+      state: {
+        text: "dnd",
+        color: "#ED4245"
+      },
+      emote: "status.gif",
+      text: "impossible is for the unwilling"
     },
-    user: {
-        id: "599336348381609984",
-        username: "MrAustin",
-        discriminator: "3915",
-        nickname: "Austin",
-        nickavatar: "pfpss2.png",
-        status: {
-            state: {
-                text: "dnd",
-                color: "#ED4245"
-            },
-            emote: "status.gif",
-            text: "impossible is for the unwilling"
-        },
-        activities: [],
-        createdTimestamp: 1615924806867,
-        avatar: "pfpss.png",
-        banner: "banner.gif",
-        accentColor: "#ce3760"
-    },
-    cards: [{     
-        name: 'circle labs',
-        icon: 'circlelabs.gif',
-        description: 'Create awesome AI chatbots with its own personalities!',
-        url: 'https://discord.gg/circlelabs',
-        position: `Community Manager`,
-     }, {
-        name: 'r/Nintendo',
-        icon: 'nintendo.gif',
-        description: 'Discuss, share content, hang around, and have a good time!',
-        url: 'https://discord.gg/nintendo',
-        position: `Administrator`,
-     }, {
-        name: 'Doors Community',
-        icon: 'doors.gif',
-        description: 'Doors is a fandom community based on the doors roblox game!',
-        url: 'https://discord.gg/doors',
-        position: `Head Moderator`
-    }],
-    header: {
-        icon: `pfpss2.png`,
-        name: `Austin`,
-        buttons: {
-            Discord: `/discord`,
-            GitHub: `/github`,
-            Twitter: `/twitter`
-        }
-    },
-    metadata: {
-        title: `MrAustin`,
-        description: `I am Austin, a inspired developer and skilled moderator with experience in creating bots and managing communities on both the Discord and Minecraft platforms. Check out my website for additional information about me!`,
-        image: `pfpss2.png`,
-        domainName: `mraustin.net`,
-        color: `#f19bec`,
-        keywords: [
-            'Austin',
-            'MrAustin',
-            'MrAustin#3915',
-            'NotMrAustin',
-            'Austins Oasis',
-            'AustinSMP',
-            'AustinLmao',
-            'Minecraft',
-            'mraustin.net',
-            'Moderator',
-            'Developer',
-            'bots',
-            'r/Nintendo',
-            'circle labs',
-            'Doors Community',
-            'Ay2k'
-        ],
+    activities: [],
+    createdTimestamp: 1615924806867,
+    avatar: "pfpss.png",
+    banner: "banner.gif",
+    accentColor: "#ce3760"
+  },
+  cards: [{
+    name: 'circle labs',
+    icon: 'circlelabs.gif',
+    description: 'Create awesome AI chatbots with its own personalities!',
+    url: 'https://discord.gg/circlelabs',
+    position: `Community Manager`,
+  }, {
+    name: 'r/Nintendo',
+    icon: 'nintendo.gif',
+    description: 'Discuss, share content, hang around, and have a good time!',
+    url: 'https://discord.gg/nintendo',
+    position: `Administrator`,
+  }, {
+    name: 'Doors Community',
+    icon: 'doors.gif',
+    description: 'Doors is a fandom community based on the doors roblox game!',
+    url: 'https://discord.gg/doors',
+    position: `Head Moderator`
+  }],
+  header: {
+    icon: `pfpss2.png`,
+    name: `Austin`,
+    buttons: {
+      Discord: `/discord`,
+      GitHub: `/github`,
+      Twitter: `/twitter`
     }
+  },
+  metadata: {
+    title: `MrAustin`,
+    description: `I am Austin, a inspired developer and skilled moderator with experience in creating bots and managing communities on both the Discord and Minecraft platforms. Check out my website for additional information about me!`,
+    image: `pfpss2.png`,
+    domainName: `mraustin.net`,
+    color: `#f19bec`,
+    keywords: [
+      'Austin',
+      'MrAustin',
+      'MrAustin#3915',
+      'NotMrAustin',
+      'Austins Oasis',
+      'AustinSMP',
+      'AustinLmao',
+      'Minecraft',
+      'mraustin.net',
+      'Moderator',
+      'Developer',
+      'bots',
+      'r/Nintendo',
+      'circle labs',
+      'Doors Community',
+      'Ay2k'
+    ],
+  }
 }
